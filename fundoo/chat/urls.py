5b608b2f2fcd4d72666a4874cd659721e7a28747
HimanshuYadav200 <103418048+HimanshuYadav200@users.noreply.github.com>
2025-06-04 10:36:04 +0530

    Update urls.py

--- fundoo/chat/urls.py
+++ fundoo/chat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
-
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
     re_path(r'^$', views.chatting, name='chat'),  # Home page for chat
     re_path(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),  # Specific room
+    path('', include('fundoo.urls')),
 ]
